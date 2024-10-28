4aaacdf2f8c2fffb0b51477de104e6fea7a2adb8
leemaneul <tigers0272@gmail.com>
2024-10-28 17:16:49 +0900

    function success

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,48 +1,80 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-export default class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
+// export default class Board extends Component { 클래스 컴포넌트 -> 함수형 컴포넌트로 변경
+  
+// }
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null)); // getter: squares / setter: setSquares
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = squares => {
+    const lines = [ 
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] && 
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) return squares[a];
     }
+    return null;
   }
+  const winner = calculateWinner(squares);
+  let status;
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+  if (winner) status = `Winner: ${winner}`;
+  else status = `Current Player: ${xIsNext ? 'X' : 'O'}`;
+  
+  const handleClick = (i) => {
+    const newSquares = squares.slice(); // 기존에는 squares = squares였으나 함수형 컴포넌트로 변경할 땐 getter와 해당 변수명이 동일하지 않도록 수정
+    if (calculateWinner(newSquares)) return;
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares); // setter함수 호출해서 인수로 newSquares
+    setXIsNext(prev => !prev);
   }
-
-  renderSquare(i) {
+  
+  const renderSquare = (i) => {
     return <Square 
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={squares[i]}
+      onClick={() => handleClick(i)}
     /> // props인 value로 자식 컴포넌트인 square 컴포넌트에게 전달
   }
-
-  render() {
-    return (
-      <div>
-        <div className='status'>Next Player: X, O</div>
-        <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  
+  // render() { 클래스 컴포넌트 -> 함수형 컴포넌트로 변경
+    
+  // }
+  return (
+    <div>
+      <div className='status'>{status}</div>
+      <div className='board-row'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className='board-row'>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
+
+export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react'
 import "./Square.css";
 
-export default class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    }
-  }
-  render() {
-    return (
-      <button 
-        className='square'
-        onClick={() => { this.props.onClick() }}>
-          {this.props.value}
-      </button>
-      // 부모 컴포넌트인 Board 컴포넌트에서 props인 value를 받아와서 출력
-    )
-  }
+const Square = ({onClick, value}) => {
+  return (
+    <button 
+      className='square'
+      onClick={() => { onClick() }}>
+        {value}
+    </button>
+    // 부모 컴포넌트인 Board 컴포넌트에서 props인 value를 받아와서 출력
+  )
 }
+
+export default Square;
